c28bcbddccb1520c87857ba885bd81744be2dc8b
unknown <viola4lfe@gmail.com>
2020-05-18 17:29:55 -0400

    MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -39,6 +39,23 @@ def bubble_sort(arr):
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
+    size = len(arr)
+    output = [0] * size
+    count = [0] * 10
+    for i in range(0, size):
+        count[arr[i]] += 1
 
+    for i in range(1, 10):
+        count[i] += count[i-1]
 
+    i = size - 1
+    while i >= 0:
+        output[count[arr[i]] - 1] = arr[i]
+        count[arr[i]] -= 1
+        i -= 1
+
+    for i in range(0, size):
+        arr[i] = output[i]
+        
     return arr
+
